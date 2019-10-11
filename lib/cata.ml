153d21d5ef414751d941477d1f3781bbc1cad792
Shon Feder <shon.feder@gmail.com>
2019-10-11 12:49:01 -0400

    Add catamorphism modules

---
+++ lib/cata.ml
@@ -0,0 +1,50 @@
+(** Catamorphisms *)
+
+module type Base = sig
+  type 'a t
+  val foldr : ('a -> 'b -> 'b) -> 'b -> 'a t -> 'b
+end
+
+module type S = sig
+  include Base
+  val fold_map : m:(module Monoid.S with type t = 'm) -> f:('a -> 'm) -> 'a t -> 'm
+  val foldl : ('b -> 'a -> 'b) -> 'b -> 'a t -> 'b
+  (* TODO Folds over semigroups *)
+
+  val to_list : 'a t -> 'a list
+  val is_empty : 'a t -> bool
+  val length : 'a t -> int
+  val any : f:('a -> bool) -> 'a t -> bool
+  val mem : 'a t -> 'a -> equal:('a -> 'a -> bool) -> bool
+  (* TODO max *)
+  (* TODO min *)
+  (* TODO sum *)
+  (* TODO product *)
+end
+
+module Make (B : Base) : S with type 'a t = 'a B.t = struct
+  include B
+
+  let fold_map (type a) ~m:(module M : Monoid.S with type t = a) ~f t =
+    foldr (fun x y -> M.op (f x) y) M.unit t
+  (* TODO *)
+
+  let foldl f z0 xs =
+    let f' x k z = k (f z x) in
+    foldr f' Fun.id xs z0
+
+  let to_list t = foldr List.cons [] t
+  let is_empty t = foldr (fun _ _ -> false) true t
+  let length t = foldl (fun c _ -> c + 1) 0 t
+  let any ~f t = fold_map ~m:(module Monoid.Bool.Or) ~f t
+  let mem t x ~equal = any ~f:(fun y -> equal x y) t
+end
+
+(* This is impossible with OCamls module sytems :( *)
+(* let make (type a) foldr =
+ *   let module Base = (struct
+ *     type _ t = a
+ *     let foldr = foldr
+ *   end : Base)
+ *   in
+ *   (module Make (Base) : S) *)
