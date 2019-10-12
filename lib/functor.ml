f997e0a352f4ddd30756e60fb56f518bc91d81a1
Shon Feder <shon.feder@gmail.com>
2019-10-11 23:37:12 -0400

    Add functor for List

--- lib/functor.ml
+++ lib/functor.ml
@@ -7,3 +7,8 @@ module Option : S with type 'a t = 'a Option.t = struct
   type 'a t = 'a Option.t
   let map ~f = Option.map f
 end
+
+module List : S with type 'a t = 'a List.t = struct
+  type 'a t = 'a List.t
+  let map ~f = List.map f
+end
