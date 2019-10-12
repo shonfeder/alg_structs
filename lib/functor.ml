27f5495da1e37245e296c5b63db9fb42e069ab0c
Shon Feder <shon.feder@gmail.com>
2019-10-11 23:30:34 -0400

    Add functor for Option

--- lib/functor.ml
+++ lib/functor.ml
@@ -2,3 +2,8 @@ module type S = sig
   type 'a t
   val map : f:('a -> 'b) -> 'a t -> 'b t
 end
+
+module Option : S with type 'a t = 'a Option.t = struct
+  type 'a t = 'a Option.t
+  let map ~f = Option.map f
+end
