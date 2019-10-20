b813a1765b4e3dac6a5ced7edd2dac1eb1111b75
Shon Feder <shon.feder@gmail.com>
2019-10-19 23:17:40 -0400

    Document and make compatible with Base

--- lib/applicative.ml
+++ lib/applicative.ml
@@ -1,24 +1,41 @@
 module type Seed = sig
   include Functor.S
   val return : 'a -> 'a t
-  val app : ('a -> 'b) t -> 'a t -> 'b t
+  val apply : ('a -> 'b) t -> 'a t -> 'b t
 end
 
 module type S = sig
   include Seed
-  val lift : f:('a -> 'b -> 'c) -> 'a t -> 'b t -> 'c t
+  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
 
+  val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t
   val ( <*> ) : ('a -> 'b) t -> 'a t -> 'b t
   val ( *> )  : 'a t -> 'b t -> 'b t
   val ( <* )  : 'a t -> 'b t -> 'a t
+
+  val both : 'a t -> 'b t -> ('a * 'b) t
+
+  module Let_bind : sig
+    val (let+) : 'a t -> ('a -> 'b) -> 'b t
+    val (and+) : 'a t -> 'b t -> ('a * 'b) t
+  end
 end
 
 module Make (B : Seed) : S with type 'a t = 'a B.t  = struct
   include B
-  let lift ~f a b = app (B.map ~f a) b
-  let ( <*> ) = app
-  let ( *> ) a b = lift ~f:Fun.(flip const) a b
-  let ( <* ) a b = lift ~f:Fun.const a b
+  let map2 a b ~f = apply (B.map ~f a) b
+
+  let ( <$> ) f x = map ~f x
+  let ( <*> ) = apply
+  let ( *> ) a b = map2 ~f:Fun.(flip const) a b
+  let ( <* ) a b = map2 ~f:Fun.const a b
+
+  let both a b = map2 ~f:(fun x y -> (x, y)) a b
+
+  module Let_bind = struct
+    let (let+) x f = map ~f x
+    let (and+) = both
+  end
 end
 
 module Law (A : S) = struct
@@ -34,41 +51,35 @@ end
 
 (* Implementations *)
 
-module List = struct
-  module Seed = struct
-    include Functor.List
-    let return x = [x]
-    let app fs xs =
-      fs
-      |> List.map (fun f -> List.map f xs)
-      |> List.flatten
-  end
-
-  include Make (Seed)
+module Option = struct
+  include Make (struct
+      include Functor.Option
+      let return x = Some x
+      let apply f x = match f with
+        | Some f -> map ~f x
+        | None -> None
+    end)
 end
 
-module Option = struct
-  module Seed = struct
-    include Functor.Option
-    let return x = Some x
-    let app f x = match f with
-      | Some f -> map ~f x
-      | None -> None
-  end
-
-  include Make (Seed)
+module List = struct
+  include Make (struct
+      include Functor.List
+      let return x = [x]
+      let apply fs xs =
+        fs
+        |> List.map (fun f -> List.map f xs)
+        |> List.flatten
+    end)
 end
 
 module Array = struct
-  module Seed = struct
-    include Functor.Array
-    let return x = [|x|]
-    let app fs xs =
-      fs
-      |> Array.map (fun f -> Array.map f xs)
-      |> Array.to_list
-      |> Array.concat
-  end
-
-  include Make (Seed)
-end
+  include Make (struct
+      include Functor.Array
+      let return x = [|x|]
+      let apply fs xs =
+        fs
+        |> Array.map (fun f -> Array.map f xs)
+        |> Array.to_list
+        |> Array.concat
+    end)
+end (* Array *)
--- lib/applicative.mli
+++ lib/applicative.mli
@@ -1,22 +1,75 @@
+(** Applicative functors "allow sequencing of functorial computations"
+    ({{:https://en.wikipedia.org/wiki/Applicative_functor} wikipedia}) with
+    the limitation that "values computed by subcomputations cannot affect what
+    subsequent computations will take place"
+    ({{:https://ocaml.janestreet.com/ocaml-core/latest/doc/base/Base__/Applicative_intf/index.html}
+    Core docs}).
+
+    Assume that we've implemented {{!module-type:S} Applicative} for some
+    parametertic type ['p t]. Take two values [F x : 'a t] and [F y : 'b t] and
+    a computation represented by a function [F f : ('a -> 'b -> 'c) t]. We can
+    then use the applicative's namesake function {!val:S.apply} to sequence this
+    computation "within" type [t]:
+
+    [apply (apply f x) y : 'c t]
+
+    The "sequencing" of the computation (via application) is even more apparent
+    when we use the infix notation for [apply]:
+
+    [f <*> x <*> y]
+
+    See {{!section:implementations} Implementations} for illustrative examples. *)
+
 (** {1 Seed} *)
 
 (** The [Seed] needed to generate an implementation of {{!module-type:S}
     Applicative} *)
 module type Seed = sig
   include Functor.S
+
+  (** [return x] is the minimal value of [x] in {!type:t} *)
   val return : 'a -> 'a t
-  val app : ('a -> 'b) t -> 'a t -> 'b t
+
+  (** [apply (F f) (F x)] is [F (f x)], i.e., [f] applied to [x] in {!type:t} *)
+  val apply : ('a -> 'b) t -> 'a t -> 'b t
 end
 
 (** {1 Interface} *)
 
 module type S = sig
   include Seed
-  val lift : f:('a -> 'b -> 'c) -> 'a t -> 'b t -> 'c t
 
+  (** [map2 ~f x y] is [return f <*> x <*> y], i.e., [f] "lifted" in to
+      {!type:t} and then applied to both [x] and [y]. *)
+  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+
+  (** [f <$> x] is [map ~f x] which is the same as [return f <*> x] *)
+  val ( <$> ) : ('a -> 'b) -> 'a t -> 'b t
+
+  (** [(F f) <*> (F x)] is [F (f x)] *)
   val ( <*> ) : ('a -> 'b) t -> 'a t -> 'b t
+
+  (** [a *> b] sequences actions [a] and [b] but discards the value of [a] *)
   val ( *> )  : 'a t -> 'b t -> 'b t
+
+  (** [a <* b] sequences actions [a] and [b] but discards the value of [b] *)
   val ( <* )  : 'a t -> 'b t -> 'a t
+
+  (** [both (F a) (F b)] is [F (a, b)], i.e., it is the product of [a] and [b]
+      in [F] *)
+  val both : 'a t -> 'b t -> ('a * 'b) t
+
+  (** Binding operators for use with the OCaml 4.08 extension. See the
+      {{:https://caml.inria.fr/pub/docs/manual-ocaml/manual046.html} OCaml docs
+      on binding operators} for more information. *)
+  module Let_bind : sig
+
+    (** [let+ x = t in f x] is [map ~f t] *)
+    val (let+) : 'a t -> ('a -> 'b) -> 'b t
+
+    (** [let+ x = t and+ y = t' in f x y] is [f <$> t <*> t'] *)
+    val (and+) : 'a t -> 'b t -> ('a * 'b) t
+  end
 end
 
 (** {1 Laws} *)
@@ -51,8 +104,33 @@ end
 
 module Make (B : Seed) : S with type 'a t = 'a B.t
 
-(** {1 Implementations} *)
+(** {1 Implementations}
 
-module List : S with type 'a t = 'a List.t
+    Implementations for common data structures. See the documentation for each
+    implementation module for an illustrative example. *)
+
+(** [Option] provides sequencing of partial computations. E.g,
+
+    [# Option.((+) <$> Some 5 <*> Some 7);;]
+
+    [- : int option = Option.Some 12]
+
+    [# Option.((+) <$> None <*> Some 5);;]
+
+    [- : int option = Option.None] *)
 module Option : S with type 'a t = 'a Option.t
-module Array : S with type 'a t = 'a Array.t
+
+(** [List] defines applications that work across all possible
+    combinations of the items in the respective lists. This is often used to
+    model non-determinism.
+
+    E.g., to get all pairs that can be formed by selecting first and second
+    elements from the respective lists:
+
+    [# let x = List.NonDet.((fun x y -> (x, y)) <$> [1;2] <*> ['a';'b']);;]
+
+    [- : (int * char) list = [(1, 'a'); (1, 'b'); (2, 'a'); (2, 'b')] ] *)
+module List : S with type 'a t = 'a List.t
+
+(** As {!module:List} but using arrays. *)
+module Array  : S with type 'a t = 'a Array.t
