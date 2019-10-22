30f97a2a9f0b73c61724153d89a6c8483be1db5d
Shon Feder <shon.feder@gmail.com>
2019-10-22 08:54:52 -0400

    Remove extra word from docs of qcheck helpers

--- alg-structs-qcheck/alg_structs_qcheck.ml
+++ alg-structs-qcheck/alg_structs_qcheck.ml
@@ -3,15 +3,17 @@
 (** TODO Document *)
 module Utils = Utils
 
-(** QCheck tests of laws {{!module:Alg_structs.Functor.Law} Functor Laws} *)
+(** QCheck tests of {{!module:Alg_structs.Functor.Law} Functor Laws} *)
 module Functor = Functor
 
-(** QCheck tests of laws {{!module:Alg_structs.Applicative.Law} Applicative Laws} *)
+(** QCheck tests of {{!module:Alg_structs.Applicative.Law} Applicative Laws} *)
 module Applicative = Applicative
 
-(** QCheck tests of laws {{!module:Alg_structs.Semigroup.Law} Semigroup Laws} *)
+(** QCheck tests of {{!module:Alg_structs.Semigroup.Law} Semigroup Laws} *)
 module Semigroup = Semigroup
 
 (** QCheck tests of {{!module:Alg_structs.Monoid.Law} Monoid Laws} *)
 module Monoid = Monoid
+
+(** QCheck tests of {{!module:Alg_structs.Foldable.Law} Foldable Laws} *)
 module Foldable = Foldable
