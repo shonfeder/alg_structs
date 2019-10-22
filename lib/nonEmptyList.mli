34963201ceea57b1ec0695872140a6a04accd8cf
Shon Feder <shon.feder@gmail.com>
2019-10-22 09:09:28 -0400

    Document non-empty list

--- lib/nonEmptyList.mli
+++ lib/nonEmptyList.mli
@@ -1,14 +1,37 @@
-(** TODO *)
+(** Non-empty lists *)
 
+(** The type of lists which cannot be empty. *)
 type 'a t
+
 include Functor.S with type 'a t := 'a t
 
+(** See [List.equal]. *)
 val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
+
+(** See [List.compare]. *)
 val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
+
+(** See [List.cons]. *)
 val cons : 'a -> 'a t -> 'a t
+
+(** [uncons t] is [(List.hd t, List.tl t)]. *)
 val uncons : 'a t -> ('a * 'a list)
+
+(** [hd t] is the first element of [t].
+
+    Since [t] is non-empty, it is guaranteed that there will always be a head. *)
 val hd : 'a t -> 'a
+
+(** [tl t] are the elements of [t] after, but not including the head.
+
+    Since [t] is non-empty, it is guaranteed that there will always be a tail. *)
 val tl : 'a t -> 'a list
+
+(** See [List.append]. *)
 val append : 'a t -> 'a t -> 'a t
+
+(** An alias for {!val:append}. *)
 val op : 'a t -> 'a t -> 'a t
+
+(** See [List.concat]. *)
 val concat : ('a -> 'a -> 'a) -> 'a t -> 'a
